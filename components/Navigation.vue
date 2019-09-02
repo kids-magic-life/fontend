0b4d9cd6ff5baee1d7849579b7fa7554cad86bcb
Hao Lin Law <49018951+lawhaolin@users.noreply.github.com>
2019-09-02 12:36:44 +1000

    initial test

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -13,24 +13,50 @@
         </div>
 
         <ul class="nav-menu">
-          <li><nuxt-link to="/">Home</nuxt-link></li>
-          <li><nuxt-link to="/">About Us</nuxt-link></li>
-          <li><nuxt-link to="/">Live Shows</nuxt-link></li>
+          <li>
+            <nuxt-link to="/">Home</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">About Us</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">Live Shows</nuxt-link>
+          </li>
           <li>
             <a href="#">Education</a>
             <ul>
-              <li><nuxt-link to="/">Life Coaching</nuxt-link></li>
-              <li><nuxt-link to="/">Parent Workshops</nuxt-link></li>
-              <li><nuxt-link to="/">Children Program</nuxt-link></li>
-              <li><nuxt-link to="/">Teachers / Schools</nuxt-link></li>
-              <li><nuxt-link to="/">Holiday Programs</nuxt-link></li>
-              <li><nuxt-link to="/">Kids Camp</nuxt-link></li>
+              <li>
+                <nuxt-link to="/">Life Coaching</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="/">Parent Workshops</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="/">Children Program</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="/">Teachers / Schools</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="/">Holiday Programs</nuxt-link>
+              </li>
+              <li>
+                <nuxt-link to="/">Kids Camp</nuxt-link>
+              </li>
             </ul>
           </li>
-          <li><nuxt-link to="/">Product</nuxt-link></li>
-          <li><nuxt-link to="/">Entertainers</nuxt-link></li>
-          <li><nuxt-link to="/">Blog</nuxt-link></li>
-          <li><nuxt-link to="/">Contact Us</nuxt-link></li>
+          <li>
+            <nuxt-link to="/">Product</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">Entertainers</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">Blog</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/">Contact Us</nuxt-link>
+          </li>
         </ul>
       </div>
     </div>
@@ -51,6 +77,7 @@ $toggle-width: 1200px;
 @media (max-width: $toggle-width) {
   .navbar {
     transition: 0.2s ease-out;
+
     .nav-menu {
       position: fixed;
       left: -80%;
@@ -64,24 +91,30 @@ $toggle-width: 1200px;
       padding: 24px 16px;
       box-shadow: 8px 0 10px 0 rgba(0, 0, 0, 0.05);
       transition: left 0.2s ease-out;
+
       a {
         line-height: 50px !important;
+
         &.active {
           border-top: 0;
         }
       }
+
       > li {
         display: block;
         border-bottom: 1px solid #f7f7f9;
+
         &:last-child {
           border-bottom: 0;
         }
       }
+
       ul {
         visibility: visible;
         opacity: 1;
         position: static;
         border-top: 0;
+
         li a {
           border-bottom: 0;
           line-height: 40px !important;
@@ -90,6 +123,7 @@ $toggle-width: 1200px;
     }
   }
 }
+
 .navbar {
   background-color: #fff;
   position: relative;
@@ -99,9 +133,11 @@ $toggle-width: 1200px;
   border-radius: 0;
   box-shadow: 0 1px 2px 0 rgba(50, 50, 50, 0.15);
   transition: background-color 0.3s ease-out;
+
   .container {
     border-bottom: 1px solid hsla(0, 0%, 100%, 0.1);
   }
+
   .navbar-toggle {
     display: none;
     float: left;
@@ -114,27 +150,33 @@ $toggle-width: 1200px;
     color: #9da7ad;
     font-size: 21px;
   }
+
   .logo {
     display: inline-block;
+
     img {
       height: $max-height;
       transition: height 0.2s linear;
     }
   }
+
   ul {
     list-style: none;
     padding-left: 0;
     margin-bottom: 0;
+
     li:hover > ul {
       top: 100%;
       opacity: 1;
       visibility: visible;
     }
   }
+
   .divider {
     line-height: $max-height;
     margin: 0 20px;
     position: relative;
+
     &:before {
       content: '';
       position: absolute;
@@ -146,23 +188,29 @@ $toggle-width: 1200px;
     }
   }
 }
+
 .nav-menu {
   display: inline-block;
   margin-left: 60px;
+
   > li {
     position: relative;
     display: inline-block;
     padding: 0 2px;
+
     &:last-child {
       padding-right: 0;
+
       > a {
         padding-right: 0;
       }
     }
+
     &:hover > a {
       color: $primary-color;
     }
   }
+
   a {
     display: inline-block;
     font-size: 13px;
@@ -173,23 +221,28 @@ $toggle-width: 1200px;
     line-height: $max-height;
     color: #9da7ad;
     transition: color 0.2s linear, line-height 0.2s linear;
+
     &:focus {
       color: $primary-color;
       text-decoration: none;
     }
+
     &:hover {
       color: $primary-color;
       text-decoration: none;
     }
+
     &.active {
       color: $primary-color;
     }
   }
+
   strong {
     font-weight: 600;
     font-size: 14px;
     font-family: Montserrat, sans-serif;
   }
+
   ul {
     position: absolute;
     top: 120%;
@@ -202,6 +255,7 @@ $toggle-width: 1200px;
     visibility: hidden;
     box-shadow: 0 1px 2px 0 rgba(50, 50, 50, 0.15);
     transition: opacity 0.4s ease-in-out, top 0.2s ease-in-out;
+
     li {
       a {
         font-family: Open Sans, sans-serif;
@@ -218,10 +272,12 @@ $toggle-width: 1200px;
         color: #9da7ad;
         border-bottom: 1px solid #f8f9fa;
       }
+
       &:last-child a {
         border-bottom: 0;
       }
     }
+
     a.active {
       border-top: none;
     }
