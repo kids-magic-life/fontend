003558d28599392044fd43516bf7ab36a9e275f8
Sang Nguyen <sangnn2012@gmail.com>
2019-10-02 20:59:43 +1000

    Add life-coaching page

---
+++ pages/education/life-coaching/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <h4>BOOK A FREE PHONE CONSULTATION</h4>
+    <p>
+      To register for a FREE 15 minute phone consultation, click the button
+      below then fill in your details and let us know your preferred date and
+      time for a call.
+    </p>
+    <div>
+      <a href="/contact-us/">Book A Consultation</a>
+    </div>
+    <div>
+      <h2>WELCOME TO LIFE COACHING OF KIDS MAGIC LIFE</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+        vehicula turpis, nec iaculis nisl. Praesent eros libero, fermentum vel
+        dui vel, posuere rutrum neque. Nam vehicula, sem eget pellentesque
+        aliquam, tortor.
+      </p>
+      <h3>
+        <strong>Why get a coach?</strong>
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Id velit ut tortor
+        pretium. Dolor sit amet consectetur adipiscing elit ut aliquam purus
+        sit.
+      </p>
+      <h3>
+        <strong>The many benefits of working with our coaches:</strong>
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Id velit ut tortor
+        pretium. Dolor sit amet consectetur adipiscing elit ut aliquam purus
+        sit.
+      </p>
+      <h3>
+        <strong>Why Life Coaching with Kids Magic Life</strong>
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Id velit ut tortor
+        pretium. Dolor sit amet consectetur adipiscing elit ut aliquam purus
+        sit.
+      </p>
+      <h3>
+        <strong>When and where?</strong>
+      </h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Id velit ut tortor
+        pretium. Dolor sit amet consectetur adipiscing elit ut aliquam purus
+        sit.
+      </p>
+      <p>
+        <strong>
+          <em
+            >The choice is yours. You can keep doing the same thing and produce
+            the same results OR you can team up with an experienced life coach
+            and make real changes quickly and more effectively than you could
+            possibly do on your own.</em
+          >
+        </strong>
+      </p>
+    </div>
+  </div>
+</template>
