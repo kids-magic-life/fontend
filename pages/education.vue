e5f63e36bd8fb80385ef9b44cbd8ad7906f54abc
Sang Nguyen <sangnn2012@gmail.com>
2019-10-02 23:18:50 +1000

    Fix errors

--- pages/education.vue
+++ pages/education.vue
@@ -5,52 +5,153 @@
         <div class="site-breadcumb white-clr">
           <h2 class="section-title">
             <span class="light-font">Kids Magic Life </span>
-            <strong class="clr-txt">Gallery </strong>
+            <strong class="clr-txt">Education </strong>
           </h2>
           <ol class="breadcrumb breadcrumb-menubar">
-            <li><nuxt-link to="/"> Home </nuxt-link> Gallery</li>
+            <li><nuxt-link to="/"> Home </nuxt-link> Education</li>
           </ol>
         </div>
       </div>
     </section>
 
-    <section class="shop-wrap sec-space-bottom">
+    <section>
       <div class="pattern">
-        <b-img fluid-grow alt="" src="/images/white-pattern.png" />
+        <b-img fluid-grow src="/images/white-pattern.png" />
       </div>
-
       <div class="section-icon">
-        <b-img src="/images/logo.png" width="128" />
+        <b-img width="128px" src="/images/logo.png" />
       </div>
 
-      <b-container class="rel-div">
-        <b-row class="result-bar block-inline">
-          <!--          <h4 class="result-txt">Search Results <b> 1 </b></h4>-->
-          <ul class="view-tabs">
-            <li class="active">
-              <a href="#grid-view" data-toggle="tab">
-                <i class="fa fa-th"></i>
-              </a>
-            </li>
-            <li class="">
-              <a href="#list-view" data-toggle="tab">
-                <i class="fa fa-th-list"></i>
-              </a>
-            </li>
-          </ul>
+      <b-container class="rel-div" style="margin-top: 24px">
+        <b-row>
+          <b-col sm="6" class="text-right fsz-16 pt-15 xs-text">
+            <p>
+              andjaeger shank strip steak short loin kevin andouille. Venison
+              buffalo drumstick, jerky tail ground round shank bresaola sausage
+              capicola. Turkey picanha chicken, sirloin landjaeger jerky pork
+              belly flank drumstick.
+            </p>
+          </b-col>
+          <b-col sm="6" class="text-left xs-text">
+            <h2 class="section-title">
+              <nuxt-link to="/contact-us">
+                <span class="light-font">Book a</span>
+                <em>
+                  free phone consultation
+                </em>
+              </nuxt-link>
+            </h2>
+          </b-col>
         </b-row>
       </b-container>
-      <b-container>
-        <div class="tab-content shop-content">
-          <div class="tab-pane active in">
-            <div class="row">
-              <b-col v-for="i in [1, 2, 3, 4, 5]" :key="i" sm="6" md="4" lg="3">
-                <gallery-item :image="{}"></gallery-item>
-              </b-col>
-            </div>
-          </div>
+    </section>
+    <section class="short-about sec-space light-bg">
+      <b-container class="rel-div">
+        <div class="title-wrap pt-15">
+          <h2 class="fsz-20 over-txt-title">
+            <span class="light-font">
+              WELCOME TO
+            </span>
+            <strong>LIFE COACHING OF KIDS MAGIC LIFE</strong>
+          </h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc at
+            vehicula turpis, nec iaculis nisl. Praesent eros libero, fermentum
+            vel dui vel, posuere rutrum neque. Nam vehicula, sem eget
+            pellentesque aliquam, tortor.
+          </p>
         </div>
+
+        <div class="about-wrap">
+          <img
+            class="logo-img"
+            alt=""
+            src="http://event-theme.com/themes/html/naturix-html/assets/img/extra/sign-1.png"
+          />
+          <h4 class="">
+            <strong>Charise</strong> <span class="light-font"> Katz </span>
+          </h4>
+          <h5 class="clr-txt sub-title-sm">KIDS MAGIC LIFE OWNER</h5>
+        </div>
+      </b-container>
+    </section>
+
+    <section>
+      <b-container>
+        <b-row class="sec-space-top">
+          <b-col sm="6" lg="6">
+            <div>
+              <h2 class="section-title ptb-15">
+                <span class="light-font">
+                  Why get a coach?
+                </span>
+              </h2>
+              <p class="fsz-16">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Id
+                velit ut tortor pretium. Dolor sit amet consectetur adipiscing
+                elit ut aliquam purus sit.
+              </p>
+            </div>
+          </b-col>
+          <b-col sm="6" lg="6">
+            <div>
+              <h2 class="section-title ptb-15">
+                <span class="light-font">
+                  Benefits of working with our coaches:
+                </span>
+              </h2>
+              <p class="fsz-16">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Id
+                velit ut tortor pretium. Dolor sit amet consectetur adipiscing
+                elit ut aliquam purus sit.
+              </p>
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+
+    <section>
+      <b-container>
+        <b-row class="sec-space-top">
+          <b-col sm="6" lg="6">
+            <div>
+              <h2 class="section-title ptb-15">
+                <span class="light-font">
+                  When and where?
+                </span>
+              </h2>
+              <p class="fsz-16">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Id
+                velit ut tortor pretium. Dolor sit amet consectetur adipiscing
+                elit ut aliquam purus sit.
+              </p>
+            </div>
+          </b-col>
+          <b-col sm="6" lg="6">
+            <div>
+              <h2 class="section-title ptb-15">
+                <span class="light-font">
+                  Why Life Coaching with Kids Magic Life
+                </span>
+              </h2>
+              <p class="fsz-16">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Id
+                velit ut tortor pretium. Dolor sit amet consectetur adipiscing
+                elit ut aliquam purus sit.
+              </p>
+            </div>
+          </b-col>
+        </b-row>
       </b-container>
     </section>
   </main>
 </template>
+
+<script></script>
+
+<style scoped></style>
